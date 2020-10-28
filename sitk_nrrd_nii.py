254656416710d70fa95beb14f942c5746d1bed6b
mikiwang820 <61928785+mikiwang820@users.noreply.github.com>
2020-10-29 01:27:26 +0800

    Nrrd2nifti

--- sitk_nrrd_nii.py
+++ sitk_nrrd_nii.py
@@ -13,35 +13,54 @@ import nibabel as nib
 import numpy as np
 import SimpleITK as sitk 
 
+#where you want to save ouput files
 save_route = '/Users/wangmeiqi/code_and_data/HCC/ntuh/hcc_ntuh_niigz_1012'
+
+#where are your input files
 main_route = '/Users/wangmeiqi/code_and_data/HCC/ntuh/HCC_labeled'
+
 filename = 'temp'
+
 route = os.path.join(main_route, filename)
+
 train = 'train'
+
 val = 'val'
+
 train_names = []
+
 train_label_names = []
+
 val_names = []
+
 val_label_names = []
+
 data = {}
+
 data['validation'] = []
+
 dir_ = []
+
 path = []
 
 
 
-
-
 for root, dirs, files in os.walk(route):
-    #print(root)
+
     for file in files:
-        #print(file)
+        
         if file != '.DS_Store':
+            
             filename = os.path.join(root, file)
+            
             read_img = sitk.ReadImage(filename)
+            
             name = root.split("/")
+            
             output_path = os.path.join(save_route, file[:-5] + '_' + name[-1] + '.nii.gz')
+            
             sitk.WriteImage(read_img,  output_path)
+            
             print("fininshing: " + output_path)
 
 
